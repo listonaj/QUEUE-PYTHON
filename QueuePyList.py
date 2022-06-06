73a0e194e1534880b601fc5cd557d5bccf09203f
listonaj <jm.romain@outlook.com>
2022-06-05 18:08:59 -0700

    dequeue method

--- QueuePyList.py
+++ QueuePyList.py
@@ -1,38 +1,30 @@
+# Jean-Marc Romain
+# 06-05-2022
+# implementation of a queue using list in python with no specifiation of size
 
 class Queue:
     def __init__(self):
-        self.linkedList = LinkedList()
+        self.items = []
     
     def __str__(self):
-        values = [str(x) for x in self.linkedList]
+        values = [str(x) for x in self.items]
         return ' '.join(values)
-    
-    def enqueue(self, value):
-        newNode = Node(value)
-        if self.linkedList.head == None:
-            self.linkedList.head = newNode
-            self.linkedList.tail = newNode
-        else:
-            self.linkedList.tail.next = newNode
-            self.linkedList.tail = newNode
-    
+
     def isEmpty(self):
-        if self.linkedList.head == None:
+        if self.items ==[] :
             return True
         else:
             return False
     
+    def enqueue(self, value):
+        self.items.append(value)
+        return f"the element {value} has been added to the Queue"
+
     def dequeue(self):
         if self.isEmpty():
-            return "There is not any node in the Queue"
+            return "There is not any element in the Queue"
         else:
-            tempNode = self.linkedList.head
-            if self.linkedList.head == self.linkedList.tail:
-                self.linkedList.head = None
-                self.linkedList.tail = None
-            else:
-                self.linkedList.head = self.linkedList.head.next
-            return tempNode
+            return self.items.pop(0)
     
     def peek(self):
         if self.isEmpty():
--- my_queue.py
+++ my_queue.py
@@ -1,8 +1,30 @@
+#Jean-Marc Romain
+#06-05-2022
+# I have tested the implementation of the Queue using list with no limit of size 
+# the method are located in the file 'QueuePyList.py'
+
 from QueuePyList import *
 
 customqueue = Queue()
+
+print('Tetsting to see if the queue is empty')
+print(customqueue.isEmpty())
+
+
+print("\nAdding some elements to the queue")
 customqueue.enqueue(1) 
 customqueue.enqueue(3) 
 customqueue.enqueue(6) 
 customqueue.enqueue(9) 
+
+print("\nTetsing again to see if the queue is empty")
+print(customqueue.isEmpty())
+
+print("\nPrinting the Queue list created")
+print(customqueue)
+
+print("\ntesting the dequeue method")
+elemremoved = customqueue.dequeue()
+print(f"I remove the first element of the list {elemremoved}")
+print(customqueue)
 
